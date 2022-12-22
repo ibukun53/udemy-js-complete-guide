ceae9844dc031e9955cb34ed943ff5f99bc20d87
Blessing Olusanya <debbyblessing1234@gmail.com>
2022-12-22 15:03:19 +0100

    Add return value

--- 02_basics_variables_data_types_operators_functions/basics-01-starting-project/assets/scripts/app.js
+++ 02_basics_variables_data_types_operators_functions/basics-01-starting-project/assets/scripts/app.js
@@ -3,12 +3,9 @@ let currentResult = defaultReault;
 
 function add(a, b) {
   const result = a + b;
-  alert(`the result is ${result}`);
+  return result;
 }
-add(1, 2);
-add(4, 3);
-// eslint-disable-next-line no-mixed-operators
-currentResult = (currentResult + 10) * 3 / 2 - 1;
+currentResult = add(1, 2);
 
 const calculateDescription = `(${defaultReault} + 10) * 3 /2 - 1`;
 // eslint-disable-next-line no-undef
