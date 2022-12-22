68ff2bc89f65d11a5b3dfb12060c0a925d14c85a
Blessing Olusanya <debbyblessing1234@gmail.com>
2022-12-22 14:43:14 +0100

    Adding a custom function

--- 02_basics_variables_data_types_operators_functions/basics-01-starting-project/assets/scripts/app.js
+++ 02_basics_variables_data_types_operators_functions/basics-01-starting-project/assets/scripts/app.js
@@ -1,6 +1,12 @@
 const defaultReault = 0;
 let currentResult = defaultReault;
 
+function add(a, b) {
+  const result = a + b;
+  alert(`the result is ${result}`);
+}
+add(1, 2);
+add(4, 3);
 // eslint-disable-next-line no-mixed-operators
 currentResult = (currentResult + 10) * 3 / 2 - 1;
 
