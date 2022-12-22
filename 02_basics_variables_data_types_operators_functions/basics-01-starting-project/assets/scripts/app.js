cd2098b8f67490bcb17906d66fe83fdbb1344a83
Blessing Olusanya <debbyblessing1234@gmail.com>
2022-12-22 15:41:55 +0100

    user input function

--- 02_basics_variables_data_types_operators_functions/basics-01-starting-project/assets/scripts/app.js
+++ 02_basics_variables_data_types_operators_functions/basics-01-starting-project/assets/scripts/app.js
@@ -1,12 +1,11 @@
+const userInput = document.getElementById('input-number');
+const addBtn = document.getElementById('btn-add');
 const defaultReault = 0;
 let currentResult = defaultReault;
 
-function add(a, b) {
-  const result = a + b;
-  return result;
+function add() {
+  currentResult += userInput.value;
+  // eslint-disable-next-line no-undef
+  outputResult(currentResult, '');
 }
-currentResult = add(1, 2);
-
-const calculateDescription = `(${defaultReault} + 10) * 3 /2 - 1`;
-// eslint-disable-next-line no-undef
-outputResult(currentResult, calculateDescription);
+addBtn.addEventListener('click', add);
